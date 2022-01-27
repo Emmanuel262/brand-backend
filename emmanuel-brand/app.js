a6b4e94661300a7ef861a55260df005b4486a8b2
ASIFIWE Emmanuel <emmanuel2koestner@gmail.com>
2022-01-27 09:33:20 +0200

    updating app.js for trying to solve the error

--- emmanuel-brand/app.js
+++ emmanuel-brand/app.js
@@ -40,13 +40,7 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // // Allow BodyParser
 // // Allow CookiParser
 app.use(cookieParser());
-// app.use(bodyParser.json({ limit: "50mb" }));
-// app.use(express.urlencoded({ extended: false }));
-// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-// app.use(express.json({ limit: "50mb" }));
-// app.use(express.urlencoded({ limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
-// app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   bodyParser.urlencoded({
     limit: "50mb",
